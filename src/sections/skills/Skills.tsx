f114211de7acb9f396a63e33764b47ee5931a034
Renan Alves <renanlvsdv@gmail.com>
2026-06-26 14:12:28 -0300

    feat: adicionado detalhe de fundo para skill section
    - Background de texto TECHNOLOGY adicionado

--- src/sections/skills/Skills.tsx
+++ src/sections/skills/Skills.tsx
@@ -12,25 +12,35 @@ const Skills = () => {
   const [options, setOptions] = useState<optionsStack>("frontend");
 
   return (
-    <Section>
-      <Subtitle>Skills</Subtitle>
-      <div className="flex gap-6 justify-center">
-        <ButtonStack setState={() => setOptions("frontend")}>
-          Front-End
-        </ButtonStack>
-        <ButtonStack setState={() => setOptions("backend")}>
-          Back-End & Devops
-        </ButtonStack>
-        <ButtonStack setState={() => setOptions("tools")}>
-          Ferramentas
-        </ButtonStack>
-      </div>
-      <div className="mt-16 max-w-400 mx-auto min-h-75">
-        {options === "backend" && <BackDevopsOptions />}
-        {options === "frontend" && <FrontendOptions />}
-        {options === "tools" && <ToolsOptions />}
-      </div>
-    </Section>
+    <>
+      <Section>
+        <Subtitle>Skills</Subtitle>
+        <div className="flex gap-6 justify-center">
+          <ButtonStack setState={() => setOptions("frontend")}>
+            Front-End
+          </ButtonStack>
+          <ButtonStack setState={() => setOptions("backend")}>
+            Back-End & Devops
+          </ButtonStack>
+          <ButtonStack setState={() => setOptions("tools")}>
+            Ferramentas
+          </ButtonStack>
+        </div>
+        <div className="mt-16 max-w-400 mx-auto min-h-75 mb-46">
+          {options === "backend" && <BackDevopsOptions />}
+          {options === "frontend" && <FrontendOptions />}
+          {options === "tools" && <ToolsOptions />}
+        </div>
+        <div className="absolute right-0">
+          <div className="relative">
+            <span className="absolute w-36 h-36 bg-dark-grey-900 rounded-full right-[-8%] top-[-60%]"></span>
+            <span className="font-senary text-white mix-blend-difference text-9xl">
+              /TECHNOLOGY
+            </span>
+          </div>
+        </div>
+      </Section>
+    </>
   );
 };
 
