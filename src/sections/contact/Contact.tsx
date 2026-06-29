2dcff918c9cf7a3f365e0dc5c19ab880024b071a
Renan Alves <renanlvsdv@gmail.com>
2026-06-29 11:57:01 -0300

    feat: adicionado ícones de linkedin e github ao final da seção de contact

--- src/sections/contact/Contact.tsx
+++ src/sections/contact/Contact.tsx
@@ -1,12 +1,12 @@
 import { FormProvider, useForm } from "react-hook-form";
-import Section from "../../components/ui/Section";
 import InputLabel from "./components/InputLabel";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   emailValidation,
   type emailValidationType,
 } from "./schema/emailValidation";
-import { FaArrowCircleRight } from "react-icons/fa";
+import { FaArrowCircleRight, FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 const Contact = () => {
   const methods = useForm({
@@ -20,54 +20,82 @@ const Contact = () => {
   };
 
   return (
-    <div className="bg-dark-grey-900 min-h-dvh">
-      <Section>
-        <div className="bg-white w-full max-w-5xl py-10 px-11 mx-auto rounded-2xl">
-          <FormProvider {...methods}>
-            <form noValidate onSubmit={methods.handleSubmit(onSubmitEmail)}>
-              <div className="flex flex-col gap-3">
-                <InputLabel
-                  registerName="name"
-                  htmlFor="nameFor"
-                  label="Nome"
-                  type="text"
-                  error={methods.formState.errors}
-                />
-                <InputLabel
-                  error={methods.formState.errors}
-                  registerName="email"
-                  htmlFor="emailFor"
-                  label="Email"
-                  type="email"
-                />
-                <InputLabel
-                  error={methods.formState.errors}
-                  registerName="subject"
-                  htmlFor="subject"
-                  label="Assunto"
-                  type="text"
-                />
-                <InputLabel
-                  error={methods.formState.errors}
-                  registerName="message"
-                  htmlFor="message"
-                  label="Mensagem"
-                  type="text"
-                />
-              </div>
+    <div className="bg-dark-grey-900 min-h-screen">
+      <section className={`flex flex-col min-h-screen max-w-490 mx-auto`}>
+        <div className="py-24 px-16">
+          <div className="bg-white w-full max-w-5xl py-10 px-11 mx-auto rounded-2xl">
+            <FormProvider {...methods}>
+              <form noValidate onSubmit={methods.handleSubmit(onSubmitEmail)}>
+                <div className="flex flex-col gap-3">
+                  <InputLabel
+                    registerName="name"
+                    htmlFor="nameFor"
+                    label="Nome"
+                    type="text"
+                    error={methods.formState.errors}
+                  />
+                  <InputLabel
+                    error={methods.formState.errors}
+                    registerName="email"
+                    htmlFor="emailFor"
+                    label="Email"
+                    type="email"
+                  />
+                  <InputLabel
+                    error={methods.formState.errors}
+                    registerName="subject"
+                    htmlFor="subject"
+                    label="Assunto"
+                    type="text"
+                  />
+                  <InputLabel
+                    error={methods.formState.errors}
+                    registerName="message"
+                    htmlFor="message"
+                    label="Mensagem"
+                    type="text"
+                  />
+                </div>
 
-              <div className="bg-dark-grey-800 rounded-3xl w-max ml-auto hover:bg-dark-grey-900">
-                <button
-                  className="flex items-center font-secondary font-bold gap-3 ml-auto text-white mt-10 px-3 py-2 cursor-pointer"
-                  type="submit"
-                >
-                  Enviar mensagem <FaArrowCircleRight className="text-white" />
-                </button>
-              </div>
-            </form>
-          </FormProvider>
+                <div className="bg-dark-grey-800 rounded-3xl w-max ml-auto hover:bg-dark-grey-900">
+                  <button
+                    className="flex items-center font-secondary font-bold gap-3 ml-auto text-white mt-10 px-3 py-2 cursor-pointer"
+                    type="submit"
+                  >
+                    Enviar mensagem{" "}
+                    <FaArrowCircleRight className="text-white" />
+                  </button>
+                </div>
+              </form>
+            </FormProvider>
+          </div>
         </div>
-      </Section>
+
+        <div className="flex gap-2 justify-end pr-5 pb-2 h-10 mt-auto">
+          <a
+            href="https://github.com/nandev04"
+            aria-label="Github"
+            target="_blank"
+          >
+            <FaGithub
+              aria-hidden={true}
+              focusable={false}
+              className="text-white w-full h-full"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nandev04/"
+            aria-label="LinkedIn"
+            target="_blank"
+          >
+            <FaLinkedinIn
+              aria-hidden={true}
+              focusable={false}
+              className="text-white w-full h-full"
+            />
+          </a>
+        </div>
+      </section>
     </div>
   );
 };
