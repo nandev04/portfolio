385e470fc078d3df28873110de445827d6f66ee9
Renan Alves <renanlvsdv@gmail.com>
2026-07-10 19:20:34 -0300

    feat: adicionar animações de rolagem e refatorar componentes Subtitle, About, FeaturedProjects e Skills

--- src/components/ui/Subtitle.tsx
+++ src/components/ui/Subtitle.tsx
@@ -1,33 +1,15 @@
-import { useLayoutEffect, useRef, type ReactNode } from "react";
+import { type ReactNode, type RefObject } from "react";
 import { FaTerminal } from "react-icons/fa";
-import { gsap } from "../../config/gsap.config";
 
-const Subtitle = ({ children }: { children: ReactNode }) => {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.fromTo(
-        wrapperRef.current,
-        { xPercent: -100 },
-        {
-          xPercent: 0,
-          scrollTrigger: {
-            start: "bottom 90%",
-            end: "bottom 60%",
-            scrub: 1.4,
-            trigger: wrapperRef.current,
-            invalidateOnRefresh: true,
-          },
-        },
-      );
-    });
-
-    return () => ctx.revert();
-  }, []);
-
+const Subtitle = ({
+  children,
+  ref,
+}: {
+  children: ReactNode;
+  ref?: RefObject<null>;
+}) => {
   return (
-    <div ref={wrapperRef} className="flex items-center gap-4 mb-16">
+    <div ref={ref} className="flex items-center gap-4 mb-16">
       <FaTerminal className="w-15 h-15 text-dark-grey-800" />
       <h2 className="font-tertiary text-dark-grey-800 font-extrabold text-6xl uppercase">
         {children}
