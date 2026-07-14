3a1bffdee65e78c3eab8613655cf4ea8139c191f
Renan Alves <renanlvsdv@gmail.com>
2026-07-14 02:02:59 -0300

    feat: dropdown de stack finalizado

---
+++ src/sections/skills/components/DropdownMenu.tsx
@@ -0,0 +1,102 @@
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  MenuSeparator,
+} from "@headlessui/react";
+import type { optionsStack } from "../Skills";
+import { RiCloseLine } from "react-icons/ri";
+import { MdScreenshotMonitor } from "react-icons/md";
+import { HiMiniServerStack } from "react-icons/hi2";
+import { FaCheck, FaChevronDown, FaTools } from "react-icons/fa";
+
+const DropdownMenu = ({
+  option,
+  setState,
+}: {
+  option: optionsStack;
+  setState: React.Dispatch<React.SetStateAction<optionsStack>>;
+}) => {
+  return (
+    <Menu>
+      <div className="relative">
+        <MenuButton className="rounded-4xl bg-dark-grey-800 px-4 py-2 text-sm font-medium text-white shadow-md h-9 data-hover:bg-dark-grey-800 data-open:bg-dark-grey-800">
+          <div className="flex gap-5.5 w-full h-full">
+            {option === "frontend" && <MdScreenshotMonitor size={"100%"} />}
+            {option === "backend" && <HiMiniServerStack size={"100%"} />}
+            {option === "tools" && <FaTools size={"100%"} />}
+            <FaChevronDown size={"100%"} />
+          </div>
+        </MenuButton>
+        <MenuItems
+          transition
+          modal={false}
+          className="absolute top-0 right-0 z-50 w-52 origin-top-right rounded-3xl bg-dark-grey-800 p-4 pt-5 text-sm text-white shadow-lg focus:outline-none transition ease-out data-closed:scale-90 data-closed:opacity-0 data-enter:duration-200 data-leave:duration-150"
+        >
+          <MenuItem>
+            {({ close }) => (
+              <button className="absolute right-2 top-3" onClick={close}>
+                <RiCloseLine size={28} strokeWidth={0.765} />
+              </button>
+            )}
+          </MenuItem>
+
+          <span className="uppercase ml-0.5 tracking-wider text-white opacity-15 text-[12px] font-default">
+            Stack
+          </span>
+
+          <div className="my-1.5">
+            <MenuItem>
+              <span
+                onClick={() => setState("frontend")}
+                className="flex gap-1.5 items-center rounded-md px-2.5 py-3 data-focus:bg-white/10"
+              >
+                <MdScreenshotMonitor />
+                Front-End
+                {option === "frontend" && (
+                  <FaCheck className="ml-auto opacity-20 size-2.5" />
+                )}
+              </span>
+            </MenuItem>
+
+            <MenuSeparator className="w-[90%] my-1 mx-auto h-px bg-white opacity-10" />
+
+            <MenuItem>
+              <span
+                onClick={() => setState("backend")}
+                className="flex gap-1.5 items-center rounded-md px-2.5 py-2 data-focus:bg-white/10"
+              >
+                <HiMiniServerStack />
+                Back-End
+                {option === "backend" && (
+                  <FaCheck className="ml-auto opacity-20 size-2.5" />
+                )}
+              </span>
+            </MenuItem>
+
+            <MenuSeparator className="w-[90%] my-1 mx-auto h-px bg-white opacity-10" />
+
+            <MenuItem>
+              <span
+                onClick={() => setState("tools")}
+                className="flex gap-1.5 items-center rounded-md px-2.5 py-2 data-focus:bg-white/10"
+              >
+                <FaTools />
+                Ferramentas
+                {option === "tools" && (
+                  <FaCheck
+                    className="ml-auto opacity-20 size-2.5"
+                    opacity={20}
+                  />
+                )}
+              </span>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </div>
+    </Menu>
+  );
+};
+
+export default DropdownMenu;
