34de115fb7723d0a23ad8a8a809c2e5e81d3f9ac
Renan Alves <renanlvsdv@gmail.com>
2026-07-16 05:27:56 -0300

    fix: remover configuração de ScrollTrigger para ignorar redimensionamento em dispositivos móveis
    - Causando bug no Carousel

--- src/config/gsap.config.ts
+++ src/config/gsap.config.ts
@@ -2,6 +2,5 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-ScrollTrigger.config({ ignoreMobileResize: true });
 
 export { gsap, ScrollTrigger };
