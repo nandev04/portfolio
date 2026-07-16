7ff03a9b4ede7b612c2fd7710ba8c9d140bd2f4c
Renan Alves <renanlvsdv@gmail.com>
2026-07-16 05:01:25 -0300

    fix: configurar ScrollTrigger para ignorar redimensionamento em dispositivos móveis

--- src/config/gsap.config.ts
+++ src/config/gsap.config.ts
@@ -2,5 +2,6 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.config({ ignoreMobileResize: true });
 
 export { gsap, ScrollTrigger };
